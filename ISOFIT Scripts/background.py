665790b8464c7702dadc95d579f97b81771d677c
rishisankar <rishi.sankar@gmail.com>
2018-07-12 12:06:57 -0700

    Added background.py

---
+++ ISOFIT Scripts/background.py
@@ -0,0 +1,27 @@
+import os
+import pyraf
+from pyraf import iraf
+
+def getBgrd(tablename):
+    iraf.tables()
+    dumpfile = tablename + '_dump.txt'
+
+    try:
+        os.remove(dumpfile)
+    except OSError:
+        pass
+
+    iraf.tdump(table=tablename,columns='INTENS',datafile=dumpfile)
+
+    file = open(dumpfile,"r")
+    lineList = file.readlines()
+    lastLine = lineList[-1]
+    lastLine = lastLine.replace('\t', '')
+    lastLine = lastLine.replace('\s', '')
+    lastLine = lastLine.replace('\n', '')
+    bg = float(lastLine)
+    file.close()
+
+    os.remove(dumpfile)
+
+    return bg
