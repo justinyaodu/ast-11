83efb996a838a9ba76d561cc347de83eec40f908
rishisankar <rishi.sankar@gmail.com>
2018-07-12 15:38:39 -0700

    Fixed python version bug

--- ISOFIT Scripts/isofit_model.sh
+++ ISOFIT Scripts/isofit_model.sh
@@ -11,17 +11,17 @@ modtab=$1"_mod.tab"
 modfits=$1"_mod.fits"
 modsubfits=$1"_modsub.fits"
 
-fitsdump=$(python -c "import fits_center; fits_center.getFitsInfo(\"$filename\")")
+fitsdump=$(python2 -c "import fits_center; fits_center.getFitsInfo(\"$filename\")")
 fitsarr=($fitsdump)
 x0=${fitsarr[0]}
 y0=${fitsarr[1]}
 maxsma=${fitsarr[2]}
 
-posangle=$(python -c "import posAngle; posAngle.getPosAng(\"$1\", \"gal_list.cat\")")
+posangle=$(python2 -c "import posAngle; posAngle.getPosAng(\"$1\", \"gal_list.cat\")")
 
 ./isofit_cl_p1.sh $x0 $y0 $posangle $maxsma $filename $modtab
 
-bgdump=$(python -c "import background; background.getBgrd(\"$modtab\")")
+bgdump=$(python2 -c "import background; background.getBgrd(\"$modtab\")")
 bgarr=($bgdump)
 bg=${bgarr[${#bgarr[@]}-1]}
 
