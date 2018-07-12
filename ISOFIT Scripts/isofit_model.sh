4e070db076f7be9c5d603fc3ff83a1bd041bdb35
gwang0136 <35745514+gwang0136@users.noreply.github.com>
2018-07-12 12:27:18 -0700

    Update isofit_model.sh

--- ISOFIT Scripts/isofit_model.sh
+++ ISOFIT Scripts/isofit_model.sh
@@ -1,7 +1,8 @@
 if [ "$#" -ne 4 ]; then
     echo "Usage: ./isofit_model.sh <GAL_NAME> <MAX_SMA> <BGR> <PA0>"
 fi
-
+galname = "$(echo $1 | cut-d'_' -f1)"
+filter = "$(echo $1 | cut-d'_' -f2)"
 filename=$1".fits"
 modtab=$1"_mod.tab"
 modfits=$1"_mod.fits"
@@ -11,6 +12,7 @@ bgdump=$(python -c "import background; background.getBgrd(\"$modtab\")")
 bgarr=($bgdump)
 bg-${bgarr[${#bgarr[@]}-1]}
 
+posangle = $(python -c "import posAngle; posAngle.getPosAng(\"$galname\", \"$filter\", \"gal_list.cat\")")
 
 #TODO argument setup:
 #CALCULATE CENTER
