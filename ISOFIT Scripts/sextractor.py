c7bff0ad8f62616c17e316b0acab58b387e5f464
gwang0136 <35745514+gwang0136@users.noreply.github.com>
2018-07-12 16:12:49 -0700

    Add files via upload

---
+++ ISOFIT Scripts/sextractor.py
@@ -0,0 +1,13 @@
+import sewpy
+import pyraf
+from pyraf import iraf
+
+def sourceExtract(galaxyname):
+
+	sew = sewpy.SEW(params=["NUMBER"], 
+                    config= {"DETECT_MINAREA": 2, "DETECT_MAXAREA": 700, "DETECT_THRESH": 3, "ANALYSIS_THRESH": 5.0, "DEBLEND_MINCOUNT": 0.002, 
+                    "CHECKIMAGE_NAME": galaxyname + "_seg.fits", "CATALOG_NAME": galaxyname + "_modsub.cat", "CHECKIMAGE_TYPE": "SEGMENTATION"})
+        
+    sew(galaxyname + "_modsub.fits")
+
+    iraf.imcopy(galaxyname + "_seg.fits", filename + ".pl")
