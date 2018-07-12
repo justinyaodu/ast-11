1a559404e66092284d063e01a070206cd87d1213
rishisankar <rishi.sankar@gmail.com>
2018-07-12 16:39:55 -0700

    Fixed sextractor.py bugs

--- ISOFIT Scripts/sextractor.py
+++ ISOFIT Scripts/sextractor.py
@@ -4,8 +4,7 @@ from pyraf import iraf
 
 def sourceExtract(galaxyname):
 	sew = sewpy.SEW(params=["NUMBER"],
-	config= {"DETECT_MINAREA": 2, "DETECT_MAXAREA": 700, "DETECT_THRESH": 3,
-	 "ANALYSIS_THRESH": 5.0, "DEBLEND_MINCOUNT": 0.002, "CHECKIMAGE_NAME": galaxyname + "_seg.fits",
-	 "CATALOG_NAME": galaxyname + "_modsub.cat", "CHECKIMAGE_TYPE": "SEGMENTATION"})
+	config= {"DETECT_MINAREA": 50, "DETECT_THRESH": 3.0,
+	"CHECKIMAGE_NAME": galaxyname + "_seg.fits", "CHECKIMAGE_TYPE": "SEGMENTATION"})
 	sew(galaxyname + "_modsub.fits")
-	iraf.imcopy(galaxyname + "_seg.fits", filename + ".pl")
+	iraf.imcopy(galaxyname + "_seg.fits", galaxyname + ".fits.pl")
