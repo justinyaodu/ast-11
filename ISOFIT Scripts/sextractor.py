c080c11c02b040b27f856588fdadecb63dbccb2e
rishisankar <rishi.sankar@gmail.com>
2018-07-13 14:16:06 -0700

    Update sextractor.py

--- ISOFIT Scripts/sextractor.py
+++ ISOFIT Scripts/sextractor.py
@@ -5,6 +5,7 @@ from pyraf import iraf
 def sourceExtract(galaxyname):
 	sew = sewpy.SEW(params=["NUMBER"],
 	config= {"DETECT_MINAREA": 50, "DETECT_THRESH": 3.0,
-	"CHECKIMAGE_NAME": galaxyname + "_seg.fits", "CHECKIMAGE_TYPE": "SEGMENTATION"})
+	"CHECKIMAGE_NAME": galaxyname + "_seg.fits", "CHECKIMAGE_TYPE": "SEGMENTATION",
+	"WEIGHT_TYPE": "MAP_RMS", "WEIGHT_IMAGE": galaxyname + "_sig.fits"})
 	sew(galaxyname + "_modsub.fits")
 	iraf.imcopy(galaxyname + "_seg.fits", galaxyname + ".fits.pl")
