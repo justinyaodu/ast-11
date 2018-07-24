496fcbb39e3f5eb8e4b84b3500cbb064acb6602c
gwang0136 <35745514+gwang0136@users.noreply.github.com>
2018-07-24 10:46:09 -0700

    Update posAngle.py

--- ISOFIT Scripts/posAngle.py
+++ ISOFIT Scripts/posAngle.py
@@ -7,20 +7,69 @@ def getPosAng(galname, gallist):
     gal = split[0]
     filt = split[1]
     parsedfil = []
+    u_ind = -1
+    g_ind = -1
+    i_ind = -1
+    z_ind = -1
     for line in file:
         parsedfil.append(np.array(re.split("\s+", line)))
-        #print(re.split("\s+", line))
     parsedfile = np.array(parsedfil)
+    for line in parsedfile:
+        if(line[1] == 'Name'):
+            for x in range(len(line)):
+                if(line[x] == 'u_PA'):
+                    u_ind = x
+                    g_ind = x+1
+                    i_ind = x+3
+                    z_ind = x+4
     pa = 0
     for line in parsedfile:
         if(line[1] == gal):
             if(filt == "u"):
-                pa = int(float(line[13]))
+                pa = int(float(line[u_ind]))
             elif(filt == "g"):
-                pa = int(float(line[14]))
+                pa = int(float(line[g_ind]))
             elif(filt == "i"):
-                pa = int(float(line[16]))
+                pa = int(float(line[i_ind]))
             elif(filt == "z"):
-                pa = int(float(line[17]))
+                pa = int(float(line[z_ind]))
     print(pa)
+    file.close()
+    
+    
+def getSMA(galname, gallist):
+    file = open(gallist, "r")
+    split = galname.split("_")
+    gal = split[0]
+    filt = split[1]
+    parsedfil = []
+    u_ind = -1
+    g_ind = -1
+    i_ind = -1
+    z_ind = -1
+    for line in file:
+        parsedfil.append(np.array(re.split("\s+", line)))
+    parsedfile = np.array(parsedfil)
+    for line in parsedfile:
+        if(line[1] == 'Name'):
+            for x in range(len(line)):
+                if(line[x] == 'u_Re(arcsec)'):
+                    u_ind = x
+                    g_ind = x+1
+                    i_ind = x+3
+                    z_ind = x+4
+    rad = 0
+    for line in parsedfile:
+        if(line[1] == gal):
+            if(filt == "u"):
+                rad = int(float(line[u_ind]))
+            elif(filt == "g"):
+                rad = int(float(line[g_ind]))
+            elif(filt == "i"):
+                rad= int(float(line[i_ind]))
+            elif(filt == "z"):
+                rad = int(float(line[z_ind]))
+    rad /= 0.187
+    rad *= 5
+    print(rad)
     file.close()
