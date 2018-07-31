289b54b765bac4cfe368eff74ba76b5059b1a648
rishisankar <rishi.sankar@gmail.com>
2018-07-31 13:25:08 -0700

    Update create_runscript.py

--- create_runscript.py
+++ create_runscript.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
     for filename in os.listdir("."):
         if filename.endswith("_sig.fits"):
             continue
-        file = filename[":-5"]
-        f.write("./isofit_model.sh " + file + " remove\n")
+        if fileame.endnswith(".fits"):
+            file = filename[:-5]
+            f.write("./isofit_model.sh " + file + " remove\n")
     f.write("\n")
     f.close()
