41f3a1244adf27dd3da20b0d3e1b413e4f38a8f9
22emilyz <52179986+22emilyz@users.noreply.github.com>
2019-07-15 15:28:06 -0700

    Update excessive_mask_check.py

--- scripts/excessive_mask_check.py
+++ scripts/excessive_mask_check.py
@@ -3,8 +3,8 @@ import re
 import numpy as np
 import fits_center
 
-  #DOCUMENTATION
-def count_original_pixel(mask_image):
+  #check to see how much of the isophote is masked
+def check_masking(mask_image):
   row_num,x_cor,y_cor,ellipticity,pos_ang,sma = np.loadtxt(sys.stdin, unpack=True)
   
   #check for the INDEF replaced value of -10000 and replace with reasonable values to check
@@ -14,16 +14,44 @@ def count_original_pixel(mask_image):
   ellipticity[ellipticity == -10000] = 0
   pos_ang[pos_ang == -10000] = 0
   
-  #using the isophotes
+  #using the isophotes to get the percentage of masked/total
   #index is the index to go through each array value
+  #countMasked is to count how many pixels in ellipse are masked
   #count is to count how many of the isophotes pass the test (if enough don't pass, then the mask = bad)
+  #countEllipse is to count the number of pixels being tested
   index=0
+  countMasked=0
   count=0
-  while(index < len(x_cor)):
-    
-    
-  
-  
-  
-  #0=masked, not 0 = not masked
-  
+  countEllipse=0
+  height,width = fits_center.fits_size(mask_image)
+  while index < len(x_cor):
+    for x in range(0,height):
+      for y in range(0,width):
+        if point_in_ellipse(x_cor[index],y_cor[index],ellipticity[index],pos_ang[index],sma[index],x,y):
+          countEllipse++
+          if data[y,x]!=0:
+              countMasked++  
+    if countMasked/countEllipse <= 0.4:      
+      count++
+    index++  
+  percentage = count/len(x_cor) 
+  if percentage > 0.7: 
+    sys.exit(0)
+  else:
+    sys.exit(1)
+                  
+  #test to see if the tested point is in the isophote ellipse                   
+def point_in_ellipse(x0, y0, ell, pa, smaj, x, y):
+    # calculate semi-minor axis length
+    smin = smaj * (1 - ell)
+
+    # translate everything so that ellipse is centered at the origin
+    x -= x0
+    y -= y0
+
+    # convert position angle to rotation CCW from x-axis
+    angle = 90 + pa
+
+    # check whether point is in rotated ellipse
+    return (math.pow(x * math.cos(angle) + y * math.sin(angle), 2) / math.pow(smaj, 2)
+          + math.pow(x * math.sin(angle) - y * math.cos(angle), 2) / math.pow(smin, 2)) < 1
