3a18be7c9cbce5ebb5aac8ca5bf32a692b1597bb
Justin Yao Du <justinyaodu@gmail.com>
2019-07-16 09:27:09 -0700

    added image parameter to excessive-mask-check.sh

--- scripts/excessive-mask-check.sh
+++ scripts/excessive-mask-check.sh
@@ -3,13 +3,14 @@
 source common.sh
 
 # print usage message if number of parameters is incorrect
-[ $# -eq 1 ] || abort "usage: $0 <table.tab>"
+[ $# -eq 1 ] || abort "usage: $0 <table.tab> <mask.fits>"
 
 table="$1"
+mask_image="$2"
 
 # abort if input file doesn't exist
-assert_exists "$table"
+assert_exists "$table" "$mask_image"
 
 # call the Python script
-./tprint.sh "$table" "X0,Y0,ELLIP,PA,SMA" | sed -e "s/INDEF/-10000/g" | python -c "import excessive_mask_check; excessive_mask_check.check_masking()"
+./tprint.sh "$table" "X0,Y0,ELLIP,PA,SMA" | sed -e "s/INDEF/-10000/g" | python -c "import excessive_mask_check; excessive_mask_check.check_masking(\"$mask_image\")"
   
