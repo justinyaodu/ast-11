068b748fd4e50bacef0377e27bb3b052f4f05585
Justin Yao Du <justinyaodu@gmail.com>
2019-07-17 14:20:36 -0700

    enabled k-sigma clipping using nclip parameter in template-samplepar.sh

--- scripts/template-samplepar.sh
+++ scripts/template-samplepar.sh
@@ -12,7 +12,7 @@ cat > uparmisesamplr.par << EOF
 integrmode,s,h,"bi-linear",bi-linear|mean|median,,"area integration mode"
 usclip,r,h,3.,0.,INDEF,"sigma-clip criterion for upper deviant points"
 lsclip,r,h,3.,0.,INDEF,"sigma-clip criterion for lower deviant points"
-nclip,i,h,0,0,INDEF,"number of sigma-clip iterations"
+nclip,i,h,1,0,INDEF,"number of sigma-clip iterations"
 fflag,r,h,0.5,0.,1.,"acceptable fraction of flagged data points "
 sdevice,s,h,"none",|none|stdgraph|stdplot|stdvdm,,"graphics device for ploting intensity samples"
 tsample,s,h,"none",,,"tables with intensity samples"
