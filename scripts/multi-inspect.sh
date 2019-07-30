c8055acdb97000f4357875fc0a3eeaf2351ca55d
Justin Yao Du <justinyaodu@gmail.com>
2019-07-30 11:48:12 -0700

    added skipping functionality to multi-inspect.sh

--- scripts/multi-inspect.sh
+++ scripts/multi-inspect.sh
@@ -20,10 +20,19 @@ if ! ds9_xpa_running; then
 	while ! ds9_xpa_running; do sleep 1; done
 fi
 
+search_for=''
+
 # loop over all modsub2 images
-for image in $(find "$containing_dir" | grep "modsub2.fits"); do
+for image in $(find "$containing_dir" | grep 'modsub2.fits'); do
+	# if image does not match search string
+	if ! grep -q "$search_for" <<< "$image"; then
+		echo "    skipping $image: does not match search string $search_for"
+		continue
+	fi
+
 	assert_successful ./inspect.sh "$image"
 
 	# press Enter to view next image
-	read -p "current: $image (press Enter)"
+	echo "current: $image"
+	read -p "enter search string, or press Enter for next: " search_for
 done
