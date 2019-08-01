685a6517742b821804e298641316aead6628d2de
22emilyz <22emilyz@students.harker.org>
2019-08-01 15:40:57 -0700

    added new 2d arrays

--- scripts/open_cat_files.py
+++ scripts/open_cat_files.py
@@ -2,10 +2,16 @@
 import astropy
 from astropy.io import fits
 import sys
+import numpy
 
 def read_catalog(catalog_file_name):
   hdul=fits.open(catalog_file_name)
   data=hdul[1].data
-  print(data)
+  arr=numpy.zeros(30,30) 
+  for x in range(30):
+	for y in range(30):
+		seperate_data=data[y]
+		arr[x][y]=seperate_data[y]
   hdul.info()
+  print(arr[0])
   sys.exit(0)
