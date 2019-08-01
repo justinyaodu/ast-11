997dcc6309ef1732f4d558ed3b4384825294f619
22emilyz <52179986+22emilyz@users.noreply.github.com>
2019-08-01 10:38:33 -0700

    Create open-cat-files.sh

---
+++ scripts/open-cat-files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# open catalog files
+
+source common.sh
+
+# print usage message if number of parameters is incorrect
+[ $# -eq 1 ] || abort "usage: $0 <galaxyname_NGVS_cat.fits>"
+
+catalog_file="$1"
+
+# abort if input file doesn't exist
+assert_exists "$catalog_file"
+
+# call Python script
+python -c "open_cat_files; open_cat_files.read_catalog()"
