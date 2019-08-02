029d38b2d9c14a0a801c3660dc4dae7d302b4573
22emilyz <52179986+22emilyz@users.noreply.github.com>
2019-08-02 11:23:45 -0700

    just a little test YAY

--- scripts/open_cat_files.py
+++ scripts/open_cat_files.py
@@ -11,13 +11,13 @@ def read_catalog(catalog_file_name):
   data=hdul[1].data
   arr_indexes=[0,1,2,3,5,6,7,8,9,11,12,13,14,15,28]
   index=0
-  for x in range(164)
+  for x in range(1,166)
 	sep_data=data[x]
 	for y in arr_indexes
 		current_arr=arr[index]
 		current_arr[index]=sep_data[y]
 		index=index+1
-  for ind in range(1) #change later !!!!
+  print(x_sex)
   #hdul.info()
   #print(ra)
   sys.exit(0)
