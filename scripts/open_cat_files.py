2679a431e09cbafc6ea05804225f14596660b77a
22emilyz <22emilyz@students.harker.org>
2019-08-02 11:07:29 -0700

    edited code to make our desired catalog table

--- scripts/open_cat_files.py
+++ scripts/open_cat_files.py
@@ -5,12 +5,19 @@ import sys
 import numpy
 
 def read_catalog(catalog_file_name):
-  ra,dec,umag,gmag,rmag,imag,zmag,umagerr,gmagerr,rmagerr,imagerr,zmagerr,p_gc=([] for i in range(13))
+  x_sex,y_sex,ra,dec,umag,gmag,rmag,imag,zmag,umagerr,gmagerr,rmagerr,imagerr,zmagerr,p_gc=([] for i in range(15))
+  arr=[x_sex,y_sex,ra,dec,umag,gmag,rmag,imag,zmag,umagerr,gmagerr,rmagerr,imagerr,zmagerr,p_gc]
   hdul=fits.open(catalog_file_name)
   data=hdul[1].data
-  for x in range(31):
+  arr_indexes=[0,1,2,3,5,6,7,8,9,11,12,13,14,15,28]
+  index=0
+  for x in range(164)
 	sep_data=data[x]
-	ra.append(sep_data[2])
+	for y in arr_indexes
+		current_arr=arr[index]
+		current_arr[index]=sep_data[y]
+		index=index+1
+  for ind in range(1) #change later !!!!
   #hdul.info()
-  print(ra)
+  #print(ra)
   sys.exit(0)
