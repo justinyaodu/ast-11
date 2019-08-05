136018c4113e48184b6ed211378244134e81e9ad
22emilyz <52179986+22emilyz@users.noreply.github.com>
2019-08-05 10:22:47 -0700

    changed the name, edited some code to open sextractor catalog files

--- scripts/open_catalogs.py
+++ scripts/open_catalogs.py
@@ -4,9 +4,10 @@
 import astropy
 from astropy.io import fits
 import sys
-import numpy
+import numpy as np
 
-def read_catalog(catalog_file_name):
+def open_catalog(catalog_file_namesextractor_catalog):
+  	
   x_sex,y_sex,ra,dec,umag,gmag,rmag,imag,zmag,umagerr,gmagerr,rmagerr,imagerr,zmagerr,p_gc=([] for i in range(15))
   arr=[x_sex,y_sex,ra,dec,umag,gmag,rmag,imag,zmag,umagerr,gmagerr,rmagerr,imagerr,zmagerr,p_gc]
   hdul=fits.open(catalog_file_name)
@@ -25,5 +26,7 @@ def read_catalog(catalog_file_name):
 	print (str(number) + "      " + str(x_sex[x]) + "  " + str(y_sex[x]) + "  " + str(ra[x]) + "  " + str(dec[x]) + "  " + str(umag[x]) + "  " + str(gmag[x]) + "  " + str(rmag[x])  + "  " + str(imag[x])  + "  " + str(zmag[x]) + "  " + str(umagerr[x]) + "  " + str(gmagerr[x]) + "  " + str(rmagerr[x]) + "  " + str(imagerr[x]) + "  " + str(zmagerr[x]) + "  " + str(p_gc[x]))
 	print ("\n")
 	number=number+1
+  #opening the sextractor catalog	
+  s_nmb,s_x_image,s_y_image,s_alpha,s_delta,s_a_world,s_erra_world,s_b_world,s_errb_world,s_theta_pa,s_errtheta_pa,s_elongation,s_ellip,s_mag_auto,s_magerr_auto,s_mag_best,s_magerr_best,s_mag_iso,s_magerr_iso,s_mag_isocor,s_magerr_isocor,s_mag_petro,s_magerr_petro,s_mag_aper,magerr_aper,s_flux_rad,s_kron_rad,s_petro_rad,s_mu_max,s_background,s_isoarea_image,s_fwhm_image,s_flags,s_class_star=np.loadtxt(sextractor_catalog,unpack=True)
   #hdul.info()
   sys.exit(0)
