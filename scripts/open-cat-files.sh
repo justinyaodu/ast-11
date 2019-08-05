b3fc0d09115823de7775d426a5d37f60e12875f5
22emilyz <52179986+22emilyz@users.noreply.github.com>
2019-08-05 10:28:49 -0700

    updated the sh to mirror the py file edits

--- scripts/open-cat-files.sh
+++ scripts/open-cat-files.sh
@@ -5,12 +5,13 @@
 source common.sh
 
 # print usage message if number of parameters is incorrect
-[ $# -eq 1 ] || abort "usage: $0 <galaxyname_NGVS_cat.fits>"
+[ $# -eq 2 ] || abort "usage: $0 <galaxyname_NGVS_cat.fits> <sextractor_catalog_file>"
 
-catalog_file="$1"
+fits_catalog_file="$1"
+sex_catalog_file="$2"
 
 # abort if input file doesn't exist
-assert_exists "$catalog_file"
+assert_exists "$fits_catalog_file" "$sex_catalog_file"
 
 # call Python script
-python -c "import open_cat_files; open_cat_files.read_catalog(\"$catalog_file\")"
+python -c "import open_cat_files; open_cat_files.read_catalog(\"$fits_catalog_file\",\"$sex_catalog_file\")"
